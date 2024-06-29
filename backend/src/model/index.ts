b925aa43a0cb44a68e2937470830a39984909f96
Mario Banovac <mario@tahoma.hr>
2024-06-29 11:53:40 +0200

    create model methods

---
+++ backend/src/model/index.ts
@@ -0,0 +1,35 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient()
+
+interface IUserInput {
+  email: string,
+  username: string,
+  firstName: string,
+  lastName: string
+}
+
+
+
+export const createUser = async(userInput: IUserInput) => {
+  try {
+    const user = await prisma.user.create({
+      data : userInput}) 
+      return user 
+  } catch(e) {
+    if(e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002') {
+      if(e.meta?.target) {
+        return `${e.meta?.target} is already in use`
+      }
+      return 'Something went wrong!'
+    }
+  }
+}
+
+export const findUser = async(emailToSearch: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: emailToSearch
+    }
+  })
+  return user
+}
