e47246136e6f52d1d888253703ffafa127f8a728
Mario Banovac <mario@tahoma.hr>
2024-07-02 12:32:31 +0200

    create user and edit user finished

--- backend/src/control/index.ts
+++ backend/src/control/index.ts
@@ -1,50 +1,105 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
-import { createUser, findUser } from '../model';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { createUser, findUser } from "../model";
+import { Errors } from "../utils/errors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Create a new user
-app.post('/users/new', async(req: Request, res: Response) => {
-  const { email, username, firstName, lastName } = req.body
-  if(!email || !username || !firstName || !lastName) {
-    res.status(400).json({
-      message: 'All fields are required!'
-    })
-  } else {
-    const createdUser = await createUser({ email, username, firstName, lastName })
-    res.status(200).json(createdUser)
+const getErrorStatusCode = (error: unknown): number => {
+  if (
+    error === Errors.UsernameAlreadyTaken ||
+    error === Errors.EmailAlreadyInUse
+  ) {
+    return 409;
   }
-})
+  
+  if( error === Errors.UserNotFound) {
+    return 404
+  }
+
+  if (error === Errors.ValidationError) {
+    return 400;
+  }
+  return 500;
+};
+
+// Create a new user
+app.post("/users/new", async (req: Request, res: Response) => {
+  try {
+    const { email, username, firstName, lastName } = req.body;
+    if (!email || !username || !firstName || !lastName) {
+      throw Errors.ValidationError;
+    } else {
+      const createdUser = await createUser({
+        email,
+        username,
+        firstName,
+        lastName,
+      });
+      res.status(201).json({
+        error: undefined,
+        data: createdUser,
+        success: true,
+      });
+    }
+  } catch (error) {
+    res.status(getErrorStatusCode(error)).json({
+      error,
+      data: undefined,
+      success: false,
+    });
+  }
+});
 
 // Edit a user
-app.post('/users/edit/:userId', async (req: Request, res: Response) => {
-  //...
-})
+app.post("/users/edit/:userId", async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params
+    const { email, username, firstName, lastName } = req.body;
+    if (!email || !username || !firstName || !lastName || !req.params || !userId || isNaN(parseInt(userId))) {
+      throw Errors.ValidationError;
+    }
+    const targetUser = await findUser(parseInt(userId), {
+      email,
+      username,
+      firstName,
+      lastName,
+    })
+    res.status(200).json({
+      error:undefined,
+      data: targetUser,
+      success:true
+    })
+  } catch (error) {
+    res.status(getErrorStatusCode(error)).json({
+      error,
+      data: undefined,
+      success: false,
+    });
+  }
+});
 
 // Find a user by email
-app.get('/users', async(req: Request, res: Response) => {
-  const { email} = req.body
-  if(!email) {
-    return res.status(400).json({
-      message: 'Email is required!'
-    })
-  } 
-  if(Object.keys(req.body).length > 1) {
-    return res.status(400).json({
-      message: 'Request contains extra params!'
-    })
-  }
-  const foundUser = await findUser(email)
-  res.status(200).json(foundUser)
-})
+// app.get("/users", async (req: Request, res: Response) => {
+//   const { email } = req.body;
+//   if (!email) {
+//     return res.status(400).json({
+//       message: "Email is required!",
+//     });
+//   }
+//   if (Object.keys(req.body).length > 1) {
+//     return res.status(400).json({
+//       message: "Request contains extra params!",
+//     });
+//   }
+//   const foundUser = await findUser(email);
+//   res.status(200).json(foundUser);
+// });
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.info('Server is listening on port: ', PORT)
-})
-
-
+  console.info("Server is listening on port: ", PORT);
+});
--- backend/src/model/index.ts
+++ backend/src/model/index.ts
@@ -1,4 +1,5 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { Errors } from "../utils/errors";
 export const prisma = new PrismaClient()
 
 interface IUserInput {
@@ -12,26 +13,60 @@ interface IUserInput {
 
 export const createUser = async(userInput: IUserInput) => {
   try {
+    
+    const existingUserByEmail = await prisma.user.findFirst({where:{
+      email: userInput.email
+    }})
+    
+    if(existingUserByEmail) {
+      throw Errors.EmailAlreadyInUse
+    }
+    
+    const existingUserByUsername = await prisma.user.findFirst({where: {
+      username: userInput.username
+    }})
+    
+    if(existingUserByUsername) {
+      throw Errors.UsernameAlreadyTaken
+    }
+    
     const user = await prisma.user.create({
       data : userInput}) 
       const userCopy = JSON.parse(JSON.stringify(user))
       delete userCopy.password
       return userCopy
-  } catch(e) {
-    if(e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002') {
-      if(e.meta?.target) {
-        return `${e.meta?.target} is already in use`
-      }
-      return 'Something went wrong!'
-    }
+  } catch(error) {
+    throw error
   }
 }
 
-export const findUser = async(emailToSearch: string) => {
+export const findUser = async(userId: number, newUserData: IUserInput) => {
   const user = await prisma.user.findUnique({
     where: {
-      email: emailToSearch
+      id: userId
     }
   })
-  return user
+  
+  if (!user) {
+    throw Errors.UserNotFound
+  }
+  
+  if (user.username === newUserData.username) {
+    throw Errors.UsernameAlreadyTaken
+  }
+  
+  if(user.email === newUserData.email) {
+    throw Errors.EmailAlreadyInUse
+  }
+  
+  const newUser = await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: newUserData
+  })
+  
+  const newUserCopy = JSON.parse(JSON.stringify(newUser))
+  delete newUserCopy.password
+  return newUserCopy
 }
